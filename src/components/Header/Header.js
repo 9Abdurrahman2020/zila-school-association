7221dce2fe27407f1c66bc013ea257882882d707
9abdurrahman2020 <9abdurrahman2020@gmail.com>
2021-09-26 16:21:49 +0600

    update README file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div className="header-container">
-            <h1>Zila School Association</h1>
+            <h1>Zila School Association, Chandpur</h1>
             <h4>Click 'Add as member' to add member in Zila School Association</h4>
             <h2>We need 10 members</h2>
         </div>
